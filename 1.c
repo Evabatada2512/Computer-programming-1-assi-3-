4f3da4cc0ff3ee660384da7417759e529c432ca2
Evabatada2512 <evabatada7@gmail.com>
2025-09-08 15:08:52 +0530

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+//code for printing first n natural numbers.
+{
+int i;
+printf("The first n natural numbers are:\n");
+for(i=1;i<=10;++i)
+{
+printf("%d\n",i);
+}
+return 0;
+}
+
